2f185db7f777b007e506396791017c5fe42f538a
Tiger Oakes <contact@tigeroakes.com>
2023-06-11 20:15:04 -0700

    More icons, update template

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,18 +2,18 @@
 
 object Versions {
     // Plugins
-    const val gradle = "7.1.1"
-    const val kotlin = "1.5.31"
+    const val gradle = "7.4.2"
+    const val kotlin = "1.8.21"
+    const val ksp = "$kotlin-1.0.11"
 
     // OneSignal
-    const val oneSignalPlugin = "0.14.0"
     const val oneSignal = "4.6.7"
 
     // App
     const val minSdk = 26
-    const val targetSdk = 31
-    const val buildTools = "30.0.3"
+    const val targetSdk = 33
+    const val buildTools = "33.0.2"
 
     // Blueprint
-    const val blueprint = "2.2.8"
+    const val blueprint = "2.3.5"
 }
